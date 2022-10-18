a4fd6ebe08a00eb45626130519010b54e3c0678b
apguan <apguan@gmail.com>
2022-10-18 01:17:46 -0700

    adding chat input animations

--- components/ChatInput.js
+++ components/ChatInput.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import Animated, {
+  FadeIn,
+  FadeOut,
+  ZoomInRotate,
+  ZoomOutRotate,
   useAnimatedStyle,
+  Easing,
   withTiming,
 } from "react-native-reanimated";
 import {
@@ -10,18 +15,40 @@ import {
   Platform,
   StyleSheet,
 } from "react-native";
+import { Feather, AntDesign, Entypo } from "@expo/vector-icons";
 
-const MESSAGE_ICON = require("../assets/smilie-message-icon.png");
-const DURATION = { duration: 250 };
+const DURATION = { duration: 400, easing: Easing.bezier(0.25, 0.1, 0.25, 1) };
+
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
+
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+
+const IconTray = ({ children, style, onPress = () => {} }) => {
+  return (
+    <AnimatedTouchableOpacity
+      style={[style ? style : styles.trayIcons]}
+      entering={ZoomInRotate.duration(275).damping(5)}
+      exiting={ZoomOutRotate.duration(275).damping(8)}
+      onPress={onPress}
+    >
+      {children}
+    </AnimatedTouchableOpacity>
+  );
+};
 
 export default ChatInput = () => {
+  const [textInputHeight, setTextInputHeight] = useState(0);
   const [message, setMessage] = useState("");
+  const showIconTray = Boolean(!message.length);
+
   const inputStyle = useAnimatedStyle(() => ({
-    width: withTiming(message.length ? 320 : 349, DURATION),
+    width: withTiming(message.length ? 291 : 259, DURATION),
   }));
 
-  const buttonStyle = useAnimatedStyle(() => ({
-    opacity: withTiming(message.length ? 1 : 0, DURATION),
+  const inputGrowStyle = useAnimatedStyle(() => ({
+    marginBottom: withTiming(textInputHeight > 20 ? 33 : 6),
+    height: withTiming(textInputHeight > 20 ? 72 : 36),
   }));
 
   const onSendPress = async () => {};
@@ -30,39 +57,46 @@ export default ChatInput = () => {
     setMessage(input);
   };
 
-  const _changeSize = ({ nativeEvent }) => {};
-
+  const _changeSize = ({ nativeEvent }) => {
+    setTextInputHeight(nativeEvent.contentSize.height);
+  };
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS == "ios" ? "padding" : "height"}
       keyboardVerticalOffset={60}
       style={styles.keyboardAvoidance}
     >
-      <Animated.View style={[styles.textInputContainer, inputStyle]}>
-        <TextInput
+      <Animated.View style={[styles.textInputContainer]}>
+        {showIconTray ? (
+          <>
+            <IconTray>
+              <AntDesign name="pluscircle" size={24} color="#999999" />
+            </IconTray>
+            <IconTray>
+              <Feather name="camera" size={24} color="#999999" />
+            </IconTray>
+            <IconTray>
+              <AntDesign name="picture" size={24} color="#999999" />
+            </IconTray>
+          </>
+        ) : (
+          <IconTray>
+            <Entypo name="chevron-right" size={24} color="#999999" />
+          </IconTray>
+        )}
+        <AnimatedTextInput
           multiline
           placeholder={"Type message..."}
-          style={[styles.textInput]}
+          style={[styles.textInput, inputStyle, inputGrowStyle]}
           value={message}
           onChangeText={onInputChange}
           onContentSizeChange={_changeSize}
         />
-        <TouchableOpacity style={styles.sendButton}>
-          <Animated.Image
-            style={[buttonStyle]}
-            height={23}
-            width={23}
-            source={MESSAGE_ICON}
-          />
-        </TouchableOpacity>
-        {/* <TouchableOpacity>
-          <Imaged
-            height={24}
-            width={24}
-            style={styles.icon}
-            source={MESSAGE_ICON}
-          />
-        </TouchableOpacity> */}
+        {!showIconTray && (
+          <IconTray style={[styles.sendButton]} onPress={onSendPress}>
+            <AntDesign name="arrowup" size={24} color="white" />
+          </IconTray>
+        )}
       </Animated.View>
     </KeyboardAvoidingView>
   );
@@ -73,27 +107,36 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   textInputContainer: {
-    width: "100%",
-    height: 44,
-    maxHeight: 88,
+    width: 387,
+    height: 48,
+    maxHeight: 80,
     marginHorizontal: 20,
     flexDirection: "row",
+    alignItems: "center",
   },
   textInput: {
-    width: "100%",
-    backgroundColor: "rgba(217, 217, 217, 0.2)",
-    fontSize: 16,
-    lineHeight: 18,
-    fontFamily: "satoshi-bold",
-    paddingTop: 14,
-    paddingBottom: 14,
-    paddingLeft: 20,
-    paddingRight: 54,
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    borderColor: "rgba(153, 153, 153, 0.3)",
+    fontFamily: "satoshi-regular",
+    borderWidth: 1,
+    height: 36,
+    maxHeight: 60,
+    marginVertical: 6,
+    width: 259,
+    fontSize: 14,
+    lineHeight: 16,
+    marginLeft: 6,
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingHorizontal: 12,
     borderRadius: 20,
   },
   sendButton: {
-    height: 44,
-    width: 40,
+    backgroundColor: "#5048E5",
+    borderRadius: 70,
+    height: 32,
+    width: 32,
+    left: 10,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -104,12 +147,9 @@ const styles = StyleSheet.create({
     height: 24,
     width: 26,
   },
-  buttonContainer: {
-    backgroundColor: "rgb(69, 169, 222)",
-    borderRadius: 25,
-    width: 50,
-    height: 50,
-    justifyContent: "center",
-    alignItems: "center",
+  trayIcons: {
+    margin: 6,
+    height: 24,
+    width: 24,
   },
 });
